75139ebe24b0721506b436d2eb7fd09b73eeb6b3
kristenc49 <145706608+kristenc49@users.noreply.github.com>
2024-12-05 14:48:45 -0500

    day2 p1 attempted (needs fixing)

--- Main.java
+++ Main.java
@@ -1,49 +1,52 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> fileData = getFileData("src/input.txt");
-        ArrayList<String> left = new ArrayList<>();
-        ArrayList<String> right = new ArrayList<>();
+
+        ArrayList<String> fileData = getFileData("src/Day2Input.txt");
+
+        int count = 0;
         for (int i = 0; i < fileData.size(); i++) {
-            left.add(fileData.get(i).split("   ")[0]);
-            right.add(fileData.get(i).split("   ")[1]);
-        }
+            boolean increasing = false;
+            String[] array = fileData.get(i).split(" ");
+          //  for (int j = 1; j < array.length-1; j++) {
+            int j = 1;
+            while (!increasing && j < array.length) {
+                if (Integer.parseInt(array[j]) > Integer.parseInt(array[j - 1])) {
+                    increasing = true;
+                }
+                j++;
+            }
 
-        Collections.sort(left);
-        Collections.sort(right);
+            boolean decreasing = false;
+            int x = 1;
+            while (!decreasing && x < array.length) {
+                if (Integer.parseInt(array[x]) < Integer.parseInt(array[x - 1])) {
+                    decreasing = true;
+                }
+                x++;
+            }
+            if (increasing || decreasing) {
+                int count2 = 0;
+                for (int n = 0; n < array.length - 1; n++) {
+                    int num = Math.abs(Integer.parseInt(array[n]) - Integer.parseInt(array[n+1]));
+                    if (num < 1 || num > 3) {
+                        count2++;
+                    }
+                }
 
-        int score = 0;
-        for (int i = 0; i < left.size(); i++) {
-            int count = 0;
-            for (int j = 0; j < right.size(); j++) {
-                if (Integer.parseInt(left.get(i)) == Integer.parseInt(right.get(j))) {
+                if (count2 == 0) {
                     count++;
                 }
             }
-            score += (Integer.parseInt(left.get(i)) * count);
+        //    }
         }
 
-        System.out.println(score);
+        System.out.println(count);
 
-//        ArrayList<Integer> differences = new ArrayList<>();
-//        for (int i = 0; i < left.size(); i++) {
-//            int num1 = Integer.parseInt(left.get(i));
-//            int num2 = Integer.parseInt(right.get(i));
-//            differences.add(Math.abs(num1-num2));
-//        }
-
-//        int sum = 0;
-//        for (int i = 0; i < differences.size(); i++) {
-//            sum += differences.get(i);
-//        }
-//
-//        System.out.println(sum);
     }
 
 
