0d027a6ab863620278bb6689a8a4f75fb97ae4f1
kristenc49 <145706608+kristenc49@users.noreply.github.com>
2024-12-19 14:49:51 -0500

    day4p1 continued

--- Main.java
+++ Main.java
@@ -6,68 +6,55 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<String> fileData = getFileData("src/Day1Input.txt");
-
-        int calibrated = 0;
+        int xmasCount = 0;
+        ArrayList<String> fileData = getFileData("src/day4input.txt");
+        String input = "";
         for (int i = 0; i < fileData.size(); i++) {
-            boolean operated = false;
-            String solution = "";
-            int x = 0;
-            while (!fileData.get(i).substring(x,x+1).equals(":")) {
-                solution += fileData.get(i).substring(x, x+1);
-                x++;
-            }
-
-            int numSolution = Integer.parseInt(solution);
-
-            String[] nums = fileData.get(i).substring(x + 1).split(" ");
-
-            if (multiply(nums) == numSolution) {
-                operated = true;
-            }
-
-            if (add(nums) == numSolution) {
-                operated = true;
-            }
-
-            if (operated) {
-                calibrated += numSolution;
-            }
-
+            input += fileData.get(i);
         }
 
-        System.out.println(calibrated);
+        for (int i = 0; i < input.length() - 4; i++) {
+            if (input.substring(i, i + 4).equals("XMAS") || input.substring(i, i + 4).equals("SAMX")) {
+                xmasCount++;
+            }
+        }
+
+
+        int i = 0;
+        String[][] crossword = new String[fileData.size()][fileData.get(0).length()];
+        for (int r = 0; r < crossword[0].length; r++) {
+            for (int c = 0; c < crossword.length; c++) {
+                crossword[r][c] = input.substring(i, i+1);
+                i++;
+            }
+        }
+
+        for (int r = 0; r < crossword[0].length; r++) {
+            for (int c = 0; c < crossword.length; c++) {
+                if (crossword[r][c].equals("X") && r > 4 && c > 4) {
+                       if (crossword[r-1][c-1].equals("M") && crossword[r-2][c-2].equals("A") && crossword[r-3][c-3].equals("S")) {
+                           xmasCount++;
+                       }
+                }
+
+                if (crossword[r][c].equals("X") && r + 3 < crossword[0].length && c + 3 < crossword.length) {
+                    if (crossword[r + 1][c + 1].equals("M") &&  crossword[r + 2][c + 2].equals("A") && crossword[r + 3][c + 3].equals("S")) {
+                        xmasCount++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(xmasCount);
 
     }
 
-
-    public static int multiply(String[] nums) {
-        int product = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            product += Integer.parseInt(nums[i]) * Integer.parseInt(nums[i+1]);
-        }
-
-        return product;
-    }
-
-    public static int add(String[] nums) {
-        int sum = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-             sum += Integer.parseInt(nums[i]) + Integer.parseInt(nums[i+1]);
-        }
-
-        return sum;
-    }
-
-    public boolean combos(String[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            int currentNum = Integer.parseInt(nums[i]);
-            for (int x = 0; x < nums.length; i++) {
-                currentNum * 
-            }
-        }
-    }
-
+//
+//    public void checkDiagonal() {
+//        for (int i = 0; i < 4; i++) {
+//            if ()
+//        }
+//    }
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
         try {
